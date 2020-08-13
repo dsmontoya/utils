e08570b69af2e6f2e40d6571fb5fd68a2339d407
Daniel Montoya <dsmontoyam@gmail.com>
2020-08-13 09:30:07 -0500

    copy map

--- maputils/maputils.go
+++ maputils/maputils.go
@@ -1,8 +1,39 @@
 package maputils
 
 import (
+	"reflect"
 	"strconv"
+
+	"github.com/dsmontoya/utils/reflectutils"
 )
+
+func Copy(source, destination interface{}) {
+	sourceValue := reflect.ValueOf(source)
+	destinationValue := reflect.ValueOf(destination)
+	keys := sourceValue.MapKeys()
+	for _, key := range keys {
+		var setValue reflect.Value
+		sourceKeyValue := sourceValue.MapIndex(key)
+		sourceKeyValueDeep := reflectutils.DeepValue(sourceKeyValue)
+		k := sourceKeyValueDeep.Kind()
+		if k == reflect.Map {
+			destinationKeyValue := reflect.MakeMap(sourceKeyValueDeep.Type())
+			Copy(sourceKeyValueDeep.Interface(), destinationKeyValue.Interface())
+			setValue = destinationKeyValue
+		} else if k == reflect.Array || k == reflect.Slice {
+			destinationKeyValue := reflect.MakeSlice(sourceKeyValueDeep.Type(), sourceKeyValueDeep.Len(), sourceKeyValueDeep.Cap())
+			for i := 0; i < sourceKeyValueDeep.Len(); i++ {
+				item := sourceKeyValueDeep.Index(i)
+				destinationItem := destinationKeyValue.Index(i)
+				destinationItem.Set(item)
+			}
+			setValue = destinationKeyValue
+		} else {
+			setValue = sourceKeyValue
+		}
+		destinationValue.SetMapIndex(key, setValue)
+	}
+}
 
 func StringToInterface(stringMap map[string]string) map[string]interface{} {
 	ifaceMap := map[string]interface{}{}
--- maputils/maputils_test.go
+++ maputils/maputils_test.go
@@ -1,7 +1,9 @@
 package maputils
 
 import (
+	"reflect"
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -13,5 +15,50 @@ func TestInterfaceValue(t *testing.T) {
 			"b": "b",
 		}
 		StringToInterface(m)
+	})
+}
+
+func TestCopy(t *testing.T) {
+	type post struct {
+		Title string
+		Date  time.Time
+	}
+	type user struct {
+		Name  string
+		Posts []*post
+	}
+	source := map[string]interface{}{
+		"string": "text",
+		"int":    1,
+		"array":  []int{1, 2, 3, 4},
+		"map": map[string]interface{}{
+			"bool":   true,
+			"struct": struct{}{},
+		},
+		"user": &user{
+			Name: "john",
+			Posts: []*post{{
+				Title: "A good title",
+				Date:  time.Now(),
+			}},
+		},
+	}
+	destination := map[string]interface{}{}
+	t.Run("copy", func(t *testing.T) {
+		Copy(source, destination)
+
+		if !reflect.DeepEqual(source, destination) {
+			t.Error("source and destination should be equal")
+		}
+
+		destination["int"] = 2
+		destination["map"].(map[string]interface{})["bool"] = false
+
+		if source["int"] == 2 {
+			t.Error("source[\"int\"] = 2, want 1")
+		}
+		if !source["map"].(map[string]interface{})["bool"].(bool) {
+			t.Error("source[\"map\"][\"bool\"] = false, want true")
+		}
 	})
 }
